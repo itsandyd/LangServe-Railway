4b09f8c6b72d7152a2fdf45f58a79245f0fb948c
pauseplayrepeat <mrsqueex@gmail.com>
2024-05-05 02:35:18 -0500

    updated server.py

--- app/server.py
+++ app/server.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI
 from langserve import add_routes
 from pirate_speak.chain import chain as pirate_speak_chain
-from stepback_qa_prompting.chain import chain as stepback_qa_prompting_chain
 
 
 app = FastAPI()
 
 add_routes(app, pirate_speak_chain, path="/pirate-speak")
-add_routes(app, stepback_qa_prompting_chain, path="/stepback-qa-prompting")
+
 
 
 @app.get("/")
