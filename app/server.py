a181c4155a09c4a888c9b8720c2837fde9ddfc7d
pauseplayrepeat <mrsqueex@gmail.com>
2024-05-04 23:42:38 -0500

    added rag-conversation route

--- app/server.py
+++ app/server.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from langserve import add_routes
 from pirate_speak.chain import chain as pirate_speak_chain
+from rag_conversation import chain as rag_conversation_chain
 
 
 app = FastAPI()
 
 add_routes(app, pirate_speak_chain, path="/pirate-speak")
+add_routes(app, rag_conversation_chain, path="/rag-conversation")
 
 
 @app.get("/")
